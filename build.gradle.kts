1512c68a9156818cd1cfb75c70b91caaf3ca01fa
tretikoff <tretikov.konstantin@studiomobile.ru>
2021-06-02 09:07:23 +0300

    Agglomerative clusterization

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,12 +13,16 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test-junit"))
+    testImplementation(kotlin("com.apporiented:hierarchical-clustering:1.1.0"))
+    implementation("com.apporiented:hierarchical-clustering:1.1.0")
+    implementation("com.github.haifengl:smile-kotlin:2.6.0")
+    implementation("com.github.haifengl:smile-plot:2.6.0")
 }
 
 tasks.test {
     useJUnit()
 }
 
-tasks.withType<KotlinCompile>() {
+tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
 }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,24 +1,34 @@
+import smile.clustering.hclust
+import smile.math.distance.Distance
 import java.io.File
+import java.lang.Math.sqrt
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.Paths
+import kotlin.math.pow
+
 
 val filenames = listOf(
     "chat.txt",
     "commits_check.txt",
     "commits_page.txt",
     "main_page.txt",
-    "main_page_then_chats.txt",
+//    "main_page_then_chats.txt",
 )
 
 class Main
 
 fun main() {
+    val map = mutableMapOf<String, MutableMap<String, Int>>()
+    val namesSet = HashSet<String>()
     for (filename in filenames) {
-        val content = File(Main::class.java.getResource(filename).path).readText();
-        val blocks = content.removePrefix("{").removeSuffix("}").split("}{")
-        val map = mutableMapOf<String, MutableMap<String, Int>>()
+        val cnt = File(Main::class.java.getResource(filename).path).readText();
+        val blocks = cnt.removePrefix("{").removeSuffix("}").split("}{")
         for (block in blocks) {
             val rules =
                 block.split(", ").filter { it.split("=").size > 1 && !it.startsWith("--") }.map { it.split("=")[0] }
             for (ruleIn in rules) {
+                namesSet.add(ruleIn)
                 for (ruleOut in rules) {
                     if (ruleIn == ruleOut) break
                     if (map.containsKey(ruleIn)) {
@@ -39,14 +49,38 @@ fun main() {
             for (x in entry.value.entries.sortedByDescending { it.value }.filter { it.value > threshold }) {
                 list.add(Pair("${entry.key} ${x.key}", x.value))
                 if (!printed) {
-//                    println(entry.key)
                     printed = true
                 }
-//                println("\t${x.key} ${x.value}")
             }
         }
-        for (x in list.sortedByDescending { it.second }) {
-            println("${x.first} ${x.second}")
+
+    }
+
+    val distance = Distance<String>(function = { name1, name2 ->
+        if (map.containsKey(name1) && map[name1]!!.containsKey(name2)) {
+            val count = (map[name1]!![name2]!! + 1).toDouble()
+            val x = 1.0 / count.pow(1.0 / 3.0)
+            x
+        } else {
+            1.0
         }
+    })
+
+    val names = namesSet.toList()
+    val clusters = hclust(names.toTypedArray(), distance, "single")
+    for (i in 2 until 100) {
+        val mp = mutableMapOf<Int, String>()
+        val partition = clusters.partition(i)
+        for (j in partition.indices) {
+            mp[partition[j]] = mp.getOrDefault(partition[j], "") + ", " + names[j]
+        }
+        val source: Path = Paths.get(Main::class.java.getResource(".").path.replace("/", "\\").removePrefix("\\"))
+        val dir = source.toAbsolutePath().toString() + File.separator + "clustering" + File.separator;
+        Files.createDirectories(Paths.get(dir))
+        val filePath = Paths.get("$dir$i.txt")
+        Files.deleteIfExists(filePath)
+        Files.createFile(filePath)
+        val str = mp.entries.joinToString("\n") { it.value.removePrefix(", ") }
+        Files.writeString(filePath, str)
     }
 }
